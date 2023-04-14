e5699217b26be436c212479575c6220ad1e913e8
wzy <2639809653@qq.com>
2023-04-14 17:38:24 +0800

    Site updated: 2023-04-14 17:38:24

--- js/fish.js
+++ js/fish.js
@@ -18,17 +18,17 @@ function fish() {
             this
     );
 }
-
-// 手机侧边栏默认不展开
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-    "menus_item_child"
-);
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " menus-closed";
-}
+//
+// // 手机侧边栏默认不展开
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+//     "menus_item_child"
+// );
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " menus-closed";
+// }
 
 //分类卡片隐藏
 var card_category_list = document.getElementsByClassName(
