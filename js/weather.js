bf979497468c9ce671eb7c906a63011a95587888
wzy <2639809653@qq.com>
2023-04-22 15:17:04 +0800

    Site updated: 2023-04-22 15:17:04

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FF9900",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "city": "CN101220109",
+        "left": "200",
+        "top": "10",
+        "key": "ea7a3556be6048dcafcb45e9f4b5aa45"
+    }
+}
